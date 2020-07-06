7747e8e67a67caf11f5672fb917859ca1e3dd485
Roberto Castro <roberto.ernesto.castro@gmail.com>
2020-07-05 19:52:39 -0600

    Paginas de Cuenta de Usuario e item para tab menu terminados 	modified:   src/app/app.component.html 	new file:   src/app/components/custom-tab-menu/custom-tab-menu.component.css 	new file:   src/app/components/custom-tab-menu/custom-tab-menu.component.html 	new file:   src/app/components/custom-tab-menu/custom-tab-menu.component.spec.ts 	new file:   src/app/components/custom-tab-menu/custom-tab-menu.component.ts 	new file:   src/app/components/custom-tab-menu/custom-tab-menu.module.ts 	new file:   src/app/pages/seguridad/cuenta/cambiar-password/cambiar-password.component.css 	new file:   src/app/pages/seguridad/cuenta/cambiar-password/cambiar-password.component.html 	new file:   src/app/pages/seguridad/cuenta/cambiar-password/cambiar-password.component.spec.ts 	new file:   src/app/pages/seguridad/cuenta/cambiar-password/cambiar-password.component.ts 	new file:   src/app/pages/seguridad/cuenta/cuenta-routing.module.ts 	new file:   src/app/pages/seguridad/cuenta/cuenta.component.css 	new file:   src/app/pages/seguridad/cuenta/cuenta.component.html 	new file:   src/app/pages/seguridad/cuenta/cuenta.component.spec.ts 	new file:   src/app/pages/seguridad/cuenta/cuenta.component.ts 	new file:   src/app/pages/seguridad/cuenta/cuenta.module.ts 	new file:   src/app/pages/seguridad/cuenta/mis-datos/mis-datos.component.css 	new file:   src/app/pages/seguridad/cuenta/mis-datos/mis-datos.component.html 	new file:   src/app/pages/seguridad/cuenta/mis-datos/mis-datos.component.spec.ts 	new file:   src/app/pages/seguridad/cuenta/mis-datos/mis-datos.component.ts 	modified:   src/app/pages/seguridad/seguridad-routing.module.ts

--- src/app/pages/seguridad/seguridad-routing.module.ts
+++ src/app/pages/seguridad/seguridad-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
 	{
 		path: 'roles',
 		loadChildren: () => import('./roles/roles.module').then( m => m.RolesModule )
-	}
+	},
+	{
+		path: 'mi-cuenta',
+		loadChildren: () => import('./cuenta/cuenta.module').then( m => m.CuentaModule )
+	},
 ];
 
 @NgModule({
